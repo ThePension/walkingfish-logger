0838441615a0421cbe690c5ea894ea335066381c
Nicolas Aubert <nicolas.aubert1@he-arc.ch>
2023-05-11 14:18:08 +0200

    Update SimpleLog class

--- src/main/java/ch/walkingfish/logger/walkingfishlogger/activemq/LogConsumer.java
+++ src/main/java/ch/walkingfish/logger/walkingfishlogger/activemq/LogConsumer.java
@@ -11,7 +11,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import ch.walkingfish.logger.walkingfishlogger.model.SimpleLog;
+import ch.walkingfish.logger.walkingfishlogger.model.log.SimpleLog;
 
 @Component
 public class LogConsumer {
