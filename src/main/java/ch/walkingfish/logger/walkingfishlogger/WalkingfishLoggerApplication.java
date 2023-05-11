201e5cf754c5bab8640a1e44bf979e481697d51a
Nicolas Aubert <nicolas.aubert1@he-arc.ch>
2023-05-11 11:19:31 +0200

    Initialize project

---
+++ src/main/java/ch/walkingfish/logger/walkingfishlogger/WalkingfishLoggerApplication.java
@@ -0,0 +1,13 @@
+package ch.walkingfish.logger.walkingfishlogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalkingfishLoggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalkingfishLoggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/walkingfish/logger/walkingfishlogger/WalkingfishLoggerApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.walkingfish.logger.walkingfishlogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalkingfishLoggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
